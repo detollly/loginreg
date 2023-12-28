aca82d8448032cf79b62e59c4869b5c1d5dc9920
detollly <84504245+detollly@users.noreply.github.com>
2023-12-28 23:45:33 +0000

    Revert "Secure Logout with Enhanced Error Handling for Login Page"

--- logout.php
+++ logout.php
@@ -2,26 +2,7 @@
 
 session_start();
 
-try {
-    // Validate session data
-    if (!isset($_SESSION["user_id"]) || !is_numeric($_SESSION["user_id"])) {
-        throw new Exception("Invalid session data.");
-    }
+session_destroy();
 
-    // Clear all session variables
-    $_SESSION = [];
-
-    // Destroy the session
-    session_destroy();
-
-    // Ensure that the session is completely closed
-    session_regenerate_id(true);
-
-    // Redirect to the login page
-    header("Location: login.php");
-    exit;
-} catch (Exception $e) {
-    // Handle exceptions
-    // Log the error or redirect to an error page as needed
-    die("An error occurred: " . $e->getMessage());
-}
+header("Location: index.php");
+exit;
